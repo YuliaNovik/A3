24b20a7b413abe82421e05586860ed275fbc98b2
YuliaNovik <novikovy@sheridancollege.ca>
2020-03-01 20:32:37 -0500

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,34 @@
+//Yulia Novikova 991578236
+//Create eventListener function that triggers text on click
+document.addEventListener("click", function(){
+  document.getElementById("p1").innerHTML="A tooth fell out, and left a space" ;
+  document.getElementById("p2").innerHTML="So big my tongue could touch my face." ;
+  document.getElementById("p3").innerHTML="And every time I smile, I show" ;
+  document.getElementById("p4").innerHTML="a space where something used to grow." ;
+  document.getElementById("p5").innerHTML="I miss my tooth as you can guess..." ;
+  document.getElementById("p6").innerHTML="But then, I have to brush one less!";
+});
+
+//This function hides elements
+function hide (id){
+	document.getElementById(id).style["visibility"]="hidden";
+
+//This function shows elements
+}
+function show(id){
+	document.getElementById(id).style["visibility"]="visible";
+}
+//This function makes faity inamge bidder
+function big(x){
+x.style.height= "650px";
+	x.style.width= "630px";
+}
+//This image makes fairy image smaller
+function small(x){
+	x.style.height= "350px";
+	x.style.width= "330px";
+}
+
+
+
+
